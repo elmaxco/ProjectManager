c1d24bc9b53bb67cdcfa412f0a1d3c95a69eff9d
Max Jacobsson <Max.Jacobsson1999@hotmail.com>
2025-02-17 17:40:27 +0100

    Add project files.

---
+++ Data/Entities/CustomerEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Data.Entities;
+
+internal class CustomerEntity
+{
+    public int Id { get; set; }
+    public string CustomerName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string? ContactPerson { get; set; } 
+
+}
